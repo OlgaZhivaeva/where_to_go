6c5df18f7f4d9333adec110b86e0cc48a15059d8
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-09-11 22:46:32 +0300

    Add Place model

--- places/models.py
+++ places/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
-print("hello!")
+
 class Place(models.Model):
     title = models.CharField(max_length=200)
+    description_short = models.CharField(max_length=500)
+    description_long = models.TextField()
+    lng = models.CharField(max_length=17)
+    lat = models.CharField(max_length=17)
+
 
     def __str__(self):
         return self.title
