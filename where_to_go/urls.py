d05567cf288fd330c92e9069c1e67735a146b258
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-09-13 00:33:56 +0300

    Setup media files serving

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -23,4 +23,4 @@ import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index)
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
