ae0ced56e57ff145d4abde819d5f6911c4aaa470
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-09-16 01:08:14 +0300

    Add django-tinymce

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.CharField(max_length=500)
-    description_long = models.TextField()
+    description_long = HTMLField()
     lng = models.CharField(max_length=17)
     lat = models.CharField(max_length=17)
 
