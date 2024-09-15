e220208f56cb04488be053d36b70019f70d06256
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-09-16 00:46:41 +0300

    Add verbatim

--- places/admin.py
+++ places/admin.py
@@ -1,26 +1,32 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from adminsortable2.admin import SortableAdminMixin
+from adminsortable2.admin import SortableStackedInline, SortableAdminBase
 from .models import Place, Image
 
 
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableStackedInline):
     model = Image
-    fields = ['image', 'get_preview', 'number']
+    extra = 0
+    fields = ['image', 'get_preview']
     readonly_fields = ["get_preview"]
+
     def get_preview(self, obj):
         return format_html('<img src="{}" width="auto" height="200px" />'.format(obj.image.url))
 
+
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [
         ImageInline,
     ]
 
+
 @admin.register(Image)
-class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
-    fields = ['get_preview']
+class ImageAdmin(admin.ModelAdmin):
+    fields = ['title', 'image', "get_preview"]
     readonly_fields = ["get_preview"]
 
     def get_preview(self, obj):
-        return format_html('<img src="{}" width="auto" height="200px" />'.format(obj.image.url))
+        if obj.image:
+            return format_html('<img src="{}" width="auto" height="200px" />'.format(obj.image.url))
+        return ''
--- places/models.py
+++ places/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.html import format_html
 
 
 class Place(models.Model):
@@ -9,21 +8,14 @@ class Place(models.Model):
     lng = models.CharField(max_length=17)
     lat = models.CharField(max_length=17)
 
-
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    title = models.ForeignKey(Place, on_delete=models.CASCADE,
-                                related_name='images')
+    title = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(db_index=True)
-    number = models.PositiveIntegerField()
-
-
-    def __str__(self):
-        return f'{self.number} {self.title}'
-
+    number = models.PositiveIntegerField(db_index=True, editable=True, default=0)
 
     class Meta:
         ordering = ['number']
