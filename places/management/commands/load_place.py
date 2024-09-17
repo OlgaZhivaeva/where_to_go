d5381e620cd52b12bb2389ffa50cc6086f0fbadb
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-09-18 01:06:28 +0300

    Add load place

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand, CommandError
+from places.models import Place, Image
+import os
+import requests
+from django.core.files.base import ContentFile
+
+
+class Command(BaseCommand):
+    help = 'Загружает данные в базу данных'
+
+    def add_arguments(self, parser):
+        parser.add_argument('path_to_json', type=str, help='Путь до json файла или папки с json файлами')
+
+    def handle(self, *args, **options):
+        path_to_json = options['path_to_json']
+        response = requests.get(path_to_json)
+        response.raise_for_status()
+        place_data = response.json()
+        place, created = Place.objects.get_or_create(
+            title=place_data["title"],
+            defaults={
+                "description_short": place_data["description_short"],
+                "description_long": place_data["description_long"],
+                "lng": place_data['coordinates']["lng"],
+                "lat": place_data['coordinates']["lat"],
+            },
+        )
+        if not created:
+            raise CommandError('Такая локация уже загружена')
+        for number, path_to_image in enumerate(place_data['imgs'], start=1):
+            response = requests.get(path_to_image)
+            response.raise_for_status()
+            image_name = os.path.basename(path_to_image)
+            image = ContentFile(response.content, name=image_name)
+            Image.objects.create(title=place, number=number, image=image)
+            self.stdout.write(self.style.SUCCESS(f'Картинка {image_name} успешно добавленa'))
+        self.stdout.write(self.style.SUCCESS(f'Место {place.title} успешно добавлено'))
--- places/models.py
+++ places/models.py
@@ -3,20 +3,27 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    description_short = models.CharField(max_length=500)
-    description_long = HTMLField()
-    lng = models.CharField(max_length=17)
-    lat = models.CharField(max_length=17)
+    title = models.CharField('заголовок', max_length=200, unique=True)
+    description_short = models.CharField('краткое описание', max_length=500)
+    description_long = HTMLField('полное описание')
+    lng = models.CharField('долгота', max_length=17)
+    lat = models.CharField('широта', max_length=17)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        unique_together = [['lng', 'lat']]
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+
 
 class Image(models.Model):
     title = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(db_index=True)
+    image = models.ImageField('картинка', db_index=True)
     number = models.PositiveIntegerField(db_index=True, editable=True, default=0)
 
     class Meta:
         ordering = ['number']
+        verbose_name = 'Картинка'
+        verbose_name_plural = 'Картинки'
